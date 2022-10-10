47f441966c767aefc7eb5f93785ec63b7f99fef1
FakhriAshraf <achraffakhri1998@gmail.com>
2022-10-10 23:56:33 +0200

    update concole_player.py, Ajout des commentaires et remanier le code

--- connect4/console_player.py
+++ connect4/console_player.py
@@ -3,9 +3,13 @@ from .game import Grid, Player
 
 class ConsolePlayer(Player):
     def play(self, grid: Grid) -> int:
-        print(grid)
-        column = int(input("Column value :"))
-        if column <7 and column >=0:
-            return column
+        print(grid) # Affichage de la Grid
+        chances = 3 # Initialisation des chances pour le joueur sur console
+        while(chances !=0):
+            chances -=1 # decrementation des chances
+            column = int(input("Column value :")) #Lecture de la valeur de colonne
+            if column <7 and column >=0: 
+                return column
+            else : print("Column value needs to be between 0 and 6 !!!\nYou still have ",chances," chances left")    
         
 
