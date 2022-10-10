3c9ecd207592f3a54ce1cf21b882afbd8ea10b3a
FakhriAshraf <achraffakhri1998@gmail.com>
2022-10-11 00:14:41 +0200

    Ajout des commentaires et remanier le code

--- connect4/cheater_b.py
+++ connect4/cheater_b.py
@@ -1,6 +1,12 @@
-from .game import Grid, Player
+from .game import Grid, Player, Cell
 
-
+# Ici, on cherche une cellule vide, et on replis 3 cellules
+# on reteorne le numéro de la colonne pour remplis la 4eme pour gagner 
 class CheaterB(Player):
     def play(self, grid: Grid) -> int:
-        ...
+        for line in range(grid.lines):
+            for column in range(grid.columns):
+                if grid.grid[line][column] == Cell.EMPTY:
+                    for l in range(3):
+                        grid.grid[line+l][column] = Cell.B
+                    return column
